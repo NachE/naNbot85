01a027688be3bf93dd8309a5433955f5c2681eaa
J.A. Nache <nache.nache@gmail.com>
2013-02-19 00:59:46 +0100

    I think I have found most updated code

--- naNbot85_client.py
+++ naNbot85_client.py
@@ -11,10 +11,6 @@ import urlparse
 import socket
 from time import sleep
 from BeautifulSoup import BeautifulSoup
-try:
-	import cPickle as pickle
-except:
-	import pickle
 
 
 def limpia_url(link,host):
@@ -52,7 +48,7 @@ def extrae_enlaces(url):
 
 
 	opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
-	urllib2.install_opener(opener)
+	urllib2.install_opener(opener) 
 
 	#variables necesarias para la conexion
 	#la url a la que nos conectamos se pasa como parametro a la funcion
@@ -82,7 +78,7 @@ def extrae_enlaces(url):
 			condescargas=""
 			conenlaces=""
 			for item in soup('a'):#extraemos todos los enlaces en la pagina
-				try:#comprobamos que la variable es correcta
+				try:#comprovamos que la variable es correcta
 					item['href']
 				except:#si no, la definimos como el propio host a buscar
 					item['href']="http://"+host
@@ -101,6 +97,7 @@ def extrae_enlaces(url):
 								#print "Guardamos: "+enlace
 								descargas2.append(enlace)#almacenamos el enlace en el array
 								cache.append(enlace)
+
 						else:
 							if enlaces2.count(enlace) <= 0 and cache.count(enlace) <= 0:
 								#print "Guardamos: "+enlace
@@ -164,19 +161,8 @@ def extrae_enlaces(url):
 		return urls_final #enviamos el buffer al programa principal (en este caso nada)
 
 
-def procesa_respuesta(msg):
-	print "Procesando respuesta del servidor"
-	data=pickle.loads(msg) #des-serializamos los datos
-	comando=data[0] #el index 0 contiene el comando
-	for asdf in data:
-		print asdf
-	if comando == "S":
-		print "Buscamos en:"
-		for pagina in data[1:]:
-			print pagina
 
-
-print "Contectando..."
+#pequenio codigo para conexion mediante sockets, mas adelante pudiera mejorar
 s = socket.socket()
 try:#Probamos por el puerto 8510
 	s.connect(("localhost", 8510))
@@ -185,20 +171,55 @@ except:#Si no hay nada en ese puerto, probamos por el 8511
 	print "Conexion por el puerto 8511"
 	s.connect(("localhost", 8511))
 
+print "Contectando..."
 #accion="Inicio"#accion la usaremos para los mensajes del servidor, antes de entrar en el bucle la definimos como Inicio para que el script pregunte al servidor que hacer
-data=[] #almacenaremos los datos a enviar
+cache=[] #guardaremos cierto numero de enlaces procesados para no repetir el envio de estos
+buffer=[]#declaramos un buffer donde almacenaremos descargas y enlaces para ir vaciandolo enviandoselo al servidor poco a poco
+buffer2=0#inicializamos buffer2 (bandera) indicando que aun no hemos llenado nada en el buffer
 print "---\nYeeeeeah.\n---"
 while True:
-
-	try:
-		s.settimeout(1)
-		accion=s.recv(4096)
-		procesa_respuesta(accion)
-	except socket.timeout:
-		print "El servidor no tiene nada para nosotros por lo visto, vamos a pedirle algo..."
-		data.append("Q?")
-		s.send(pickle.dumps(data,1))#Enviamos al servidor los datos
-
+	#print "------------------"
+	if len(buffer) <=0:#si el buffer esta vacio
+		if buffer2 == 1:#esto significa que el buffer esta vacio pero que ha sido llenado por lo qu el servidor aun tiene un mensaje para nosotros que ignoramos
+			#print "Recogiendo trozos perdidos en el buffer"
+			accion=s.recv(1024)#este mensaje lo ignoramos, contendra "OK" despues de enviar la ultima parte del buffer
+			#print "Comunicacion depurada: " + accion
+		#print "Q?"
+		s.send("Q?")#preguntamos al servidor que hacer
+		print "Esperando nueva pagina"
+		accion=s.recv(1024)#obtenemos la orden del servidor
+	else: #si el buffer esta lleno
+		#print "Continuando envio de datos"
+		accion=s.recv(1024) #obtenemos el mensaje del servidor ya que el el comando esta siendo enviado en la parte donde se vacia el buffer
+	#print "Servidor: "+accion
+	if accion == "QUIT": #si el servidor nos manda quit:
+		break #rompemos el bucle y salimos
+	elif accion == "WAIT": #El servidor nos comunica que debemos esperar, puede ocurrir al principio del buscador cuando aun tiene que recolectar urls suficientes o por saturacion
+		sleep(10) #esperamos 10 segundos
+		s.send("RD") #Tras esperar preguntamos al servidor si esta listo
+	elif accion == "OK":#Parte donde vaciamos el buffer. La primera vez que entramos en el nos confirma que esta listo para recibir datos, el resto confirma que los datos han llegado correctamente
+		#CUIDADO CON ESTO PUEDE PETAR AL HACERSE LA PICHA UN LIO CON EL ENVIO Y RECEPCION DE MENSAJES
+		if len(buffer) > 0:#comprobamos que el buffer no esta vacio
+			buffer2=2 #indicamos que el buffer aun esta lleno
+			try:
+				print "Enviando..."
+				s.send(buffer.pop())#enviamos el ultimo elemento del array (pop se encarga de ello)
+			except:
+				print "Error enviando parte del buffer, reintentando"
+				s.send("RD")
+			if len(buffer) <=0:
+				print "Completo (Buffer limpio)"
+				buffer2=1 #indicamos al programa principal que el buffer ya ha sido limpiado, esto le servira para recoger mensajes del servidor pendientes
+	else: #en caso contrario, nos mandara un link para explorarlo
+		#print "Buscando en " + accion
+		print accion #imprimimos la url a buscar
+		buffer[:]=[] #vaciamos el array (no se si es necesario, pero por si acaso...)
+		buffer=extrae_enlaces(accion)#en este caso la variable accion (respuesta del servidor) contiene una url que deberemos explorar
+		if len(buffer) > 0: #si alguna de las dos variables contiene algo (el bufer esta lleno). Si no se llena muy posiblemente no se pudo conectar a la url dada
+			print "Pagina procesada, listo para enviar resultados"
+			s.send("RD") #preguntamos al servidor si esta listo antes de enviarle el buffer
+		else:
+			buffer2=0
 
 print "Cliente fuera"
 s.close()
--- naNbot85_server.py
+++ naNbot85_server.py
@@ -8,13 +8,9 @@ import re
 import sys
 from time import sleep
 import MySQLdb
-try:
-	import cPickle as pickle
-except:
-	import pickle
 
 def mysql_conect():
-	db=MySQLdb.connect(host='localhost',user='user_of_naNbot85',passwd='pass_of_naNbot85',db='nanbot85')
+	db=MySQLdb.connect(host='localhost',user='user_for_naNbot85',passwd='pass_for_naNbot85',db='pydirect')
 	cursor=db.cursor()
 	return cursor
 
@@ -31,21 +27,54 @@ def aceptar_conexiones():
         pass
 
 
-#funcion que procesa los envios del cliente
-def procesa(msg):
-	data=pickle.loads(msg) #des-serializamos los datos
-	comando=data[0] #el index 0 contiene el comando
-	if comando == "Q?":
-		print "Cliente pregunta que hacer, enviamos algunas paginas"
-		data2=[]
-		data2.append("S")
-		i=0
-		while i < 900:
-			i=i+1
-			print i
-			data2.append("http://www.nada"+str(i)+".com")
-		print "Enviando" + str( len(data2) ) + "paginas"
-		return pickle.dumps(data2,1) #retornamos los datos serializados
+def guarda_enlaces(msg):
+	global cursor
+	global cache
+	if len(cache) > 2: #si en la cache hemos alcanzado los 100 comandos, procedemos a guardar, es importante tener suficentes urls a explorar, si no esto falla
+		print "Cache lleno, volcando en la base de datos..."
+		for msg2 in cache:
+			#try:#separamos cada enlace en un array ignorando los 3 primeros caractres que corresponden con "EN "
+			enlaces=msg2[3:].split(" ")
+			host=enlaces.pop(0)
+			if len(enlaces) > 0:
+				for enlace in enlaces: #vamos extraiendo los enlaces uno a uno
+					#r=cursor.execute("select * from enlaces where url='"+enlace+"'") #comprobamos si existe el enlace
+					enlace=enlace.replace("'","%27")
+					enlace=enlace.replace("\'","%27")
+					enlace=enlace.replace("//","/")
+					enlace=enlace.replace("///","/")
+					enlace=enlace.replace("////","/")
+					enlace=enlace.replace("http:/","http://")
+					consulta="insert into enlaces (url,urlorigen) values ('"+enlace+"','"+host+"')"
+					r=copyenlaces.count(enlace)
+					if r <= 0:# si el enlace no existe
+						try:
+							cursor.execute("insert into enlaces (url,urlorigen) values ('"+enlace+"','"+host+"')")
+							copyenlaces.append(enlace)
+						except:
+							print "Fallo al procesar el enlace "+enlace+ "Host: "+host
+							print "Consulta: "+consulta
+			#except: #en caso de fallo, informamos
+			#	print "No se pudieron procesar los enlaces: "+msg
+		cache[:]=[]#vaciamos la cache
+	else:
+		#print "\r Cache: %s" % (str(len(cache)))
+		cache.append(msg)
+
+
+def guarda_descargas(msg):
+	global cursor
+	try:
+		descargas=msg[3:].split(" ")
+		host=descargas.pop(0)#extraemos el host donde se encntraban los enlaces
+		if len(descargas) > 0: #si una vez extraido el comando y el host principal hay datos en "descargas" significa que son las descargas que guardaremos en la db
+			print "Descargas encontradas"
+			for descarga in descargas:#vamos extraiendo las descargas una a una
+				r=cursor.execute("select * from descargas where url='"+descarga+"'")#comprobamos si existe la descarga en la db
+				if r <= 0:#si no existe la descarga
+					cursor.execute("insert into descargas (url,origen) values ('"+descarga+"','"+host+"')")
+	except:
+		print "No se guardaron las descargas: "+msg
 
 
 #funcion que extraera enlaces de la DB segun su numero de visitas: esperemos que funcione correctamente cuando tenga un numero de clientes elevado :S
@@ -60,9 +89,67 @@ def extrae_enlace():
 	else:
 		return "http://localhost" # si algo salio mal o si no hay enlaces ordenamos al cliente que espere (copon!!)
 
-# Obsolete:
+def guarda_mime(msg):
+	global cursor
+	try:
+		print "Descarga encontrada"
+		mime=msg[3:].split(" ")
+		descarga=mime.pop(0)
+		mimetype=mime.pop(0)
+		cursor.execute("insert into descargas (url,mimetype) values ('"+descarga+"','"+mimetype+"')")
+	except:
+		print "No se Guardo la descarga con mimetype"
+
+def decide(msg):
+	if msg[:2] == "EN": #el cliente encontro enlaces
+		guarda_enlaces(msg)
+		return "OK"
+	elif msg[:2] == "DE": #el cliente encontro descargas
+		guarda_descargas(msg)
+		return "OK"
+	elif msg[:2] == "Q?": #el cliente pregunta que hacer
+		try:#extraemos un enlace
+			enlace=extrae_enlace()
+			return enlace #devolvemos el enlace para que el cliente lo explore
+		except:#si algo fue mal mandamos esperar al cliente
+			return "WAIT"
+	elif msg[:2] == "RD": #el cliente pregunta si estamos listos
+		return "OK" #Evidentemente, lo estamos. A no ser que mas adelante comprobemos la saturacion, en ese caso enviaremos un WAIT
+	elif msg[:2] == "MI":
+		guarda_mime(msg)
+		return "OK"
+	else:
+		print "Algo extranio ha pasado. Un cliente ha muerto?"
+		return "WAIT" #Si el cliente muere inesperadamente es posible que el servidor se haga la picha un lio por algun tiempo, en ese caso enviamos WAIT. Si el cliente por algun fallo no envia nada tambien enviamos WAIT. Esto es poco eficiente, habra que mejorarlo mas adelante
+
+
 # print "PyDirect Server V0.1\n(C) Juan Antonio Nache. 2008"
 print "naNbot85 Server v0.1\n(C) Juan Antonio Nache. 2008-2013"
+
+print "Conectando a la base de datos..."
+try:#no conectamos a la base de datos
+	cursor=mysql_conect()
+except:#si no pudimos lo comunicamos por pantalla y cerramos el programa
+	print "No se pudo establecer conexion con la base de datos. Cerrando"
+	sys.exit()
+
+print "Almacenando una copia de la base de datos en memoria."
+copyenlaces=[]
+copydescargas=[]
+
+print "Paso 1/2 Almacenando enlaces..."
+cursor.execute("select url from enlaces")
+resultado=cursor.fetchall()
+for result in resultado:
+	copyenlaces.append(result[0])
+print "Numero de enlaces: %s" % (str(len(copyenlaces)))
+
+print "Paso 2/2 Almacenando descargas..."
+cursor.execute("select url from descargas")
+resultado=cursor.fetchall()
+for result in resultado:
+	copydescargas.append(result[0])
+print "Numero de descargas: %s" % (str(len(copydescargas)))
 
 print "Fumando esperooo un cliente que no veooo... (Esperando clientes)"
 global server
@@ -70,29 +157,38 @@ server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 try:#Probamos por el puerto 8510
 	server.bind(("", 8510))
 	print "Puerto de escucha: 8510"
-except:#Si por el puerto anterior no podemos, probamos con el 8511
+except:#Si por el anterior no podemos, probamos con el 8511
 	server.bind(("", 8511))
 	print "Puerto de escucha: 8511"
 
-server.listen(999)#numero maximo de conexiones
+server.listen(999)
 global desc
 cache=[] #definimos la cache de todos los datos encontrados, mas tarde procesaremos todo
 desc = [server]
 while 1:
-	aceptar_conexiones() #aceptamos conexiones por un tiempo breve
+	aceptar_conexiones()
 	(sread, swrite, sexc) = select.select(desc, [], [])
 	for sock in sread:
 		if sock != server: #si el socket no es el del servidor (??)
 			try:
-				msg=sock.recv(1024)#recibimos datos del cliente
+				msg=sock.recv(1024)
 				sock.settimeout(.1)
-				respuesta=procesa(msg) #funcion que procesa el mensaje y devuelve una respuesta
-				if len(respuesta) > 0:
-					sock.send(respuesta) #se la enviamos al cliente.
+				#print "Cliente: " + msg #mensaje del cliente
+				if msg == "quit": #si el cliente quiere salir..
+					sock.send("quit") #le confirmamos la salida
+					desc.remove(sock) #y lo borramos de la lista
 				else:
-					print "Ninguna orden para el cliente"
+					respuesta=decide(msg) #funcion que decide la respuesta
+					if len(respuesta) > 0:
+						#print "Respuesta:" +respuesta
+						sock.send(respuesta) #se la enviamos al cliente.
+					else:
+						print "Ninguna orden para el cliente"
 			except:
 				#host, port=sock.getpeername()
 				#print "[%s:%s] Desconectado." % (str(host), str(port))
-				print "Un cliente ha caido o hubo error procesando mensaje del cliente"
+				print "Un cliente ha caido"
 				desc.remove(sock)
+
+
+
