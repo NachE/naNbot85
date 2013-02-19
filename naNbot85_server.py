11e2a30149284c681a336b6e39301d01e649be91
J.A. Nache <nache.nache@gmail.com>
2013-02-19 21:54:00 +0100

    rewriting server code

--- naNbot85_server.py
+++ naNbot85_server.py
@@ -9,23 +9,15 @@ import sys
 from time import sleep
 import MySQLdb
 
-def mysql_conect():
-	db=MySQLdb.connect(host='localhost',user='naNbot85',passwd='naNbot85',db='naNbot85')
-	cursor=db.cursor()
-	return cursor
 
+class naNbotDB:
+	def __init__(self):
+		print ("[+] Starting DB...")
+		db=MySQLdb.connect(host='localhost',user='naNbot85',passwd='naNbot85',db='naNbot85')
+		self.cursor=db.cursor()
 
-def aceptar_conexiones():
-    try:
-        global server
-        global desc
-        newsock, (remhost, remport) = server.accept()
-        server.settimeout(.1)
-        print "Ahaha! Nuevo cliente: %s:%s" % (str(remhost), str(remport))
-        desc.append(newsock)
-    except:
-        pass
-
+	def exec(self,query):
+		return self.cursor.execute(query)		
 
 def guarda_enlaces(msg):
 	global cursor
@@ -46,10 +38,11 @@ def guarda_enlaces(msg):
 					enlace=enlace.replace("////","/")
 					enlace=enlace.replace("http:/","http://")
 					consulta="insert into enlaces (url,urlorigen) values ('"+enlace+"','"+host+"')"
+					print (consulta)
 					r=copyenlaces.count(enlace)
 					if r <= 0:# si el enlace no existe
 						try:
-							cursor.execute("insert into enlaces (url,urlorigen) values ('"+enlace+"','"+host+"')")
+							cursor.execute(consulta)
 							copyenlaces.append(enlace)
 						except:
 							print "Fallo al procesar el enlace "+enlace+ "Host: "+host
@@ -100,28 +93,6 @@ def guarda_mime(msg):
 	except:
 		print "No se Guardo la descarga con mimetype"
 
-def decide(msg):
-	if msg[:2] == "EN": #el cliente encontro enlaces
-		guarda_enlaces(msg)
-		return "OK"
-	elif msg[:2] == "DE": #el cliente encontro descargas
-		guarda_descargas(msg)
-		return "OK"
-	elif msg[:2] == "Q?": #el cliente pregunta que hacer
-		try:#extraemos un enlace
-			enlace=extrae_enlace()
-			return enlace #devolvemos el enlace para que el cliente lo explore
-		except:#si algo fue mal mandamos esperar al cliente
-			return "WAIT"
-	elif msg[:2] == "RD": #el cliente pregunta si estamos listos
-		return "OK" #Evidentemente, lo estamos. A no ser que mas adelante comprobemos la saturacion, en ese caso enviaremos un WAIT
-	elif msg[:2] == "MI":
-		guarda_mime(msg)
-		return "OK"
-	else:
-		print "Algo extranio ha pasado. Un cliente ha muerto?"
-		return "WAIT" #Si el cliente muere inesperadamente es posible que el servidor se haga la picha un lio por algun tiempo, en ese caso enviamos WAIT. Si el cliente por algun fallo no envia nada tambien enviamos WAIT. Esto es poco eficiente, habra que mejorarlo mas adelante
-
 
 # print "PyDirect Server V0.1\n(C) Juan Antonio Nache. 2008"
 print "naNbot85 Server v0.1\n(C) Juan Antonio Nache. 2008-2013"
@@ -151,44 +122,108 @@ for result in resultado:
 	copydescargas.append(result[0])
 print "Numero de descargas: %s" % (str(len(copydescargas)))
 
-print "Fumando esperooo un cliente que no veooo... (Esperando clientes)"
-global server
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-try:#Probamos por el puerto 8510
-	server.bind(("", 8510))
-	print "Puerto de escucha: 8510"
-except:#Si por el anterior no podemos, probamos con el 8511
-	server.bind(("", 8511))
-	print "Puerto de escucha: 8511"
 
-server.listen(999)
-global desc
-cache=[] #definimos la cache de todos los datos encontrados, mas tarde procesaremos todo
-desc = [server]
-while 1:
-	aceptar_conexiones()
-	(sread, swrite, sexc) = select.select(desc, [], [])
-	for sock in sread:
-		if sock != server: #si el socket no es el del servidor (??)
-			try:
-				msg=sock.recv(1024)
-				sock.settimeout(.1)
-				#print "Cliente: " + msg #mensaje del cliente
-				if msg == "quit": #si el cliente quiere salir..
-					sock.send("quit") #le confirmamos la salida
-					desc.remove(sock) #y lo borramos de la lista
+class naNbotMain:
+	def __init__(self):
+		print "Fumando esperooo un cliente que no veooo... (Esperando clientes)"
+		self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		try:#Probamos por el puerto 8510
+			self.server.bind(("", 8510))
+			print "Puerto de escucha: 8510"
+		except:#Si por el anterior no podemos, probamos con el 8511
+			self.server.bind(("", 8511))
+			print "Puerto de escucha: 8511"
+
+		self.server.listen(999) #max connections
+		self.desc = [self.server] #store the server sock first, necesary?
+		self.cache=[] #definimos la cache de todos los datos encontrados, mas tarde procesaremos todo
+		self.IA=naNbotIA()
+
+	def mainLoop(self):
+		while 1:
+			self.waitcnx() #wait microsec for new conections
+			self.forSocks()
+	
+	def forSocks(self):
+		(sread, swrite, sexc) = select.select(self.desc, [], [])
+		for sock in sread:
+			if sock != self.server:
+				self.readSock(sock)
+
+	def readSock(self,sock):
+		try:
+			msg=sock.recv(1024)
+			sock.settimeout(.1) #necesary?
+			if msg == "quit": #if client want to quit
+				sock.send("quit") #confirm to client
+				self.desc.remove(sock) #delete from list
+			else:
+				if len(self.IA.msgClient(msg)) > 0:
+					sock.send(res) #send response to client
 				else:
-					respuesta=decide(msg) #funcion que decide la respuesta
-					if len(respuesta) > 0:
-						#print "Respuesta:" +respuesta
-						sock.send(respuesta) #se la enviamos al cliente.
-					else:
-						print "Ninguna orden para el cliente"
-			except:
-				#host, port=sock.getpeername()
-				#print "[%s:%s] Desconectado." % (str(host), str(port))
-				print "Un cliente ha caido"
-				desc.remove(sock)
+					print("No response to client.")
+		except:
+			print ("Client out") #maybe not... review this
+			self.desc.remove(sock)
+
+	def waitcnx(self):
+		try:
+			newsock, (remhost, remport) = self.server.accept()
+			self.server.settimeout(.1)
+			print "Ahaha! Nuevo cliente: %s:%s" % (str(remhost), str(remport))
+			self.desc.append(newsock)
+		except:
+			pass
+
+
+class naNbotIA:
+	def __init__(self):
+		print ("[+] Starting IA 1+1=3...")
+
+	def msgClient(msg):
+		#client found links
+		if   msg[:2] == "EN":
+			guarda_enlaces(msg)
+			return "OK"
+
+
+		#client found downloads
+		elif msg[:2] == "DE": 
+			guarda_descargas(msg)
+			return "OK"
+
+
+		#client ask what to do
+		elif msg[:2] == "Q?":
+			try:#extraemos un enlace
+				enlace=extrae_enlace()
+				return enlace #devolvemos el enlace para que el cliente lo explore
+			except:#si algo fue mal mandamos esperar al cliente
+				return "WAIT"
+
+
+		#client ask if we are ready
+		elif msg[:2] == "RD":
+			return "OK" #now always yes. Add flow control in future.
+
+
+		#dont know yet
+		elif msg[:2] == "MI":
+			guarda_mime(msg)
+			return "OK"
+
+
+		#default option
+		else:
+			print "Algo extranio ha pasado. Un cliente ha muerto?"
+			return "WAIT" 
+			#Si el cliente muere inesperadamente es posible 
+			#que el servidor se haga la picha un lio por 
+			#algun tiempo, en ese caso enviamos WAIT. Si el 
+			#cliente por algun fallo no envia nada tambien 
+			#enviamos WAIT. Esto es poco eficiente, habra 
+			#que mejorarlo mas adelante
+
 
 
 
