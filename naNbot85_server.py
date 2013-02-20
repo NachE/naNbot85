282b321823a00f43825776b602e6a37eb36279af
J.A. Nache <nache.nache@gmail.com>
2013-02-20 21:20:41 +0100

    fixes in server

--- naNbot85_server.py
+++ naNbot85_server.py
@@ -15,20 +15,40 @@ class naNbotDB:
 		self.db=MySQLdb.connect(host='localhost',user='naNbot85',
 			passwd='naNbot85',db='naNbot85')
 		self.cursor=self.db.cursor()
+		self.i=0
+		self.maxToCommit = 50 # TODO make algoritm to autocontrol this
 
-	def query(self,query):
-		print ("[i] DB exec query: %s" % query)
+	def queryInsert(self,query): #query with commit control
+		self.comitOrNot()
+		self.query(query)
+
+	def query(self,query): #query without commit control
 		try:
 			ret = self.cursor.execute(query)
 		except:
 			self.db.rollback()
 			raise #uncomment for debug
 			ret = -1
-
 		return ret
 
 	def fetchall(self):
 		return self.cursor.fetchall()
+
+	def fetchone(self):
+		return self.cursor.fetchone()
+
+	def commitOrNot(self):
+		self.i = self.i+1
+		if self.i > self.maxToCommit:
+			self.i = 0;
+			self.commit()
+
+	def commit(self):
+		print("[+] Commit db...")
+		self.db.commit() #save changes
+
+	def __del__(self):
+		self.commit()
 
 class naNbotWorker:
 	def __init__(self):
@@ -52,15 +72,13 @@ class naNbotWorker:
 	def processLinks(self,links):
 		host = links.pop(0) #extract host (first element)
 		for link in links:
-			# TODO here we clean the link
-			# TODO insert only if link does not exist
-			self.DB.query("insert into enlaces (url,urlorigen) values ('"+link+"','"+host+"')")
-			#enlace=enlace.replace("'","%27")
-			#enlace=enlace.replace("\'","%27")
-			#enlace=enlace.replace("//","/")
-			#enlace=enlace.replace("///","/")
-			#enlace=enlace.replace("////","/")
-			#enlace=enlace.replace("http:/","http://")
+			#TODO insert only if the link does not exist
+			self.DB.queryInsert("insert into enlaces (url,urlorigen) "
+					"values ('"+self.cleanLink(link)+"','"+host+"');")
+
+	def cleanLink(self,link):
+		return link.replace("'","\\'")
+
 
 	###
 	def foundDownloads(self,msg):
@@ -72,36 +90,19 @@ class naNbotWorker:
 		host = downs.pop(0)
 		for down in downs:
 			# TODO do it only if down dows not exist
-			self.DB.query("insert into descargas (url,origen)",
-					" values ('"+descarga+"','"+host+"')")
-
+			self.DB.queryInsert("insert into descargas (url,origen)"
+					 "values ('"+descarga+"','"+host+"')")
 
 	###
 	def getLink(self):
 		if self.DB.query("select url from enlaces order by explorado asc limit 1") >= 1:
-			result = self.DB.fetchall()
+			result = self.DB.fetchone()
 			self.DB.query("update enlaces set explorado=explorado+1"
-					" where url='"+result[0][0]+"'")
-			return str(result[0][0])
+					" where url='"+result[0]+"'")
+			return str(result[0])
 		else:
 			return "http://localhost" #TODO or maybe WAIT
-
 	
-"""
-This would be download also
-"""
-#	def guarda_mime(msg):
-#	global cursor
-#	try:
-#		print "Descarga encontrada"
-#		mime=msg[3:].split(" ")
-#		descarga=mime.pop(0)
-#		mimetype=mime.pop(0)
-#		cursor.execute("insert into descargas (url,mimetype)"
-#		" values ('"+descarga+"','"+mimetype+"')")
-#	except:
-#		print "No se Guardo la descarga con mimetype"
-
 class naNbotMain:
 	def __init__(self):
 		print ("[i] Opening sockets..")
@@ -120,37 +121,42 @@ class naNbotMain:
 	def mainLoop(self):
 		while 1:
 			self.waitcnx() #wait microsec for new conections
-			self.forSocks()
+			self.forSocks()#foreach socks
 	
 	def forSocks(self):
 		(sread, swrite, sexc) = select.select(self.desc, [], [])
 		for sock in sread:
 			if sock != self.server:
-				self.readSock(sock)
+				self.readSock(sock) #read every sock
 
 	def readSock(self,sock):
-		
-		msg=sock.recv(1024)
-		sock.settimeout(.1) #necesary?
-		if msg == "quit": #if client want to quit
-			sock.send("quit") #confirm to client
-			self.desc.remove(sock) #delete from list
-		else:
-			sock.send( self.IA.msgClient(msg) ) #send response to client
-	#	except:
-	#		print ("Client out") #maybe not... review this
-	#		self.desc.remove(sock)
+		try:
+			msg=sock.recv(1024)
+			sock.settimeout(.1) #necesary?
+			if msg == "quit": #if client want to quit
+				sock.send("quit") #confirm to client
+				self.desc.remove(sock) #delete from list
+			else:
+				response = self.IA.msgClient(msg)
+				print("Server say: %s" % str(response))
+				sock.send( response ) #send response to client
+
+		except socket.error, e:
+			(errnum, errmsg) = e
+			#http://docs.python.org/2/library/errno.html
+			#if errnum == EPIPE:
+			print ("[!] ERROR: %s" % errmsg)
+			self.desc.remove(sock) #sock error, remove
 
 	def waitcnx(self):
 		try:
 			newsock, (remhost, remport) = self.server.accept()
 			self.server.settimeout(.1)
-			print ("Ahaha! Nuevo cliente: %s:%s" % 
+			print ("Ahaha! New client: %s:%s" % 
 					(str(remhost), str(remport)))
 			self.desc.append(newsock)
 		except:
 			pass
-
 
 class naNbotIA:
 	def __init__(self):
@@ -159,7 +165,7 @@ class naNbotIA:
 
 
 	def msgClient(self,msg):
-		print "MSG", msg
+		print ("Client say: %s" % str(msg))
 		#client found links
 		if   msg[:2] == "EN":
 			self.worker.foundLinks(msg)
@@ -205,12 +211,7 @@ class naNbotIA:
 			"""	
 
 
-
-
-
-
 # print "PyDirect Server V0.1\n(C) Juan Antonio Nache. 2008"
 print "naNbot85 Server v0.1\n(C) Juan Antonio Nache. 2008-2013"
 naNbot85 = naNbotMain()
 naNbot85.mainLoop()
-
